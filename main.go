f44ee098f85ed48e536d57d4007cb313418cf5d8
Adrian Lanzafame <adrianlanzafame92@gmail.com>
2018-10-29 12:40:03 +1000

    initial commit

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/google/go-github/github"
+	"github.com/tidwall/gjson"
+	"github.com/tomnomnom/linkheader"
+	"golang.org/x/oauth2"
+)
+
+func main() {
+	token := os.Getenv("GITHUB_TOKEN")
+	if token == "" {
+		log.Fatal("please set GITHUB_TOKEN env var with a Personal Access Token")
+	}
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	user, _, err := client.Users.Get(ctx, "lanzafame")
+	if err != nil {
+		log.Fatal(err)
+	}
+	subsURL := *user.SubscriptionsURL
+
+	rch := make(chan string, 10)
+
+	go func() {
+		for {
+			select {
+			case repo := <-rch:
+				if !gjson.Valid(repo) {
+					log.Fatal("json response not valid")
+				}
+				result := gjson.Parse(repo)
+				result.ForEach(func(key, value gjson.Result) bool {
+					name := value.Get("full_name")
+					fmt.Println(name)
+					return true // keep iterating
+				})
+			}
+		}
+	}()
+
+	for {
+		resp, err := http.Get(subsURL)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		rch <- string(body)
+
+		links := linkheader.Parse(resp.Header.Get("link"))
+		nexts := links.FilterByRel("next")
+		next := nexts[0]
+		lasts := links.FilterByRel("last")
+		last := lasts[0]
+		if next.URL == last.URL {
+			break
+		}
+		subsURL = next.URL
+	}
+
+}
